8bfa7145030885d09dea1919a41c601f6eacb5c5
peevaliz <107015027+peevaliz@users.noreply.github.com>
2023-01-01 17:16:03 -0800

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default function App() {
           <a
             href="https://github.com/peevaliz/react-weather-project"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on GitHub
           </a>
