4df9a0c57576084f24aba846fbc72250a5868a29
peevaliz <107015027+peevaliz@users.noreply.github.com>
2023-01-01 16:36:11 -0800

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        <p>
+          This project was coded by Yelyzaveta Pieieva and it`s{" "}
+          <a
+            href="https://github.com/peevaliz/react-weather-project"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
